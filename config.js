702f7343d9ddf43834997b839f41e92bce3ffc51
Robert Friedl <rdfriedl@gmail.com>
2019-01-03 18:19:07 -0600

    add support for yaml config

--- config.js
+++ config.js
@@ -1,16 +1,37 @@
-if (process.env.CONFIG) {
-	try {
-		module.exports = JSON.parse(process.env.CONFIG);
-		console.log("Loaded config from CONFIG env variable");
-	} catch (err) {
-		console.log("Failed to parse CONFIG env variable");
-		console.log(err);
+const yaml = require("js-yaml");
+const fs = require("fs");
 
-		process.exit(1);
-	}
+let config;
+
+if (process.env.CONFIG) {
+	console.log("Loaded config from env");
+	config = process.env.CONFIG;
 } else if (process.env.CONFIG_PATH) {
-	module.exports = require(process.env.CONFIG_PATH);
 	console.log("Loaded config from " + process.env.CONFIG_PATH);
+	config = fs.readFileSync(process.env.CONFIG_PATH, "utf8");
 } else {
 	throw new Error("CONFIG or CONFIG_PATH must be set");
+}
+
+let error;
+
+if (String(config).match(/^---/)) {
+	try {
+		module.exports = yaml.safeLoad(config);
+	} catch (err) {
+		error = err;
+	}
+} else {
+	try {
+		module.exports = JSON.parse(config);
+	} catch (err) {
+		error = err;
+	}
+}
+
+if (error) {
+	console.log("Failed to parse CONFIG env variable");
+	console.log(err);
+
+	process.exit(1);
 }
